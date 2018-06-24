6a3c66476576f2b326a105037f9cfafaa0f216a9
Antti-Pekka Meronen <antice@kapsi.fi>
2018-06-24 12:48:40 +0300

    Duplicate subtitle line checking and fixing.

--- bwsrt.py
+++ bwsrt.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 
 """ A tool to fix common errors from SubRip files. """
 
@@ -21,11 +21,13 @@ from class_subrip import SubRip
 # Declare variables, read configuration, check command line arguments, etc.
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-debug = False
-fix = False
+debug = False # Debugging mode
+fix = False # Fix mode (files are not fixed by default)
 scriptspath = os.path.dirname(os.path.realpath(__file__)) # Scripts path
 configfile = os.path.join(scriptspath, "bwsrt.cfg") # Configuration file
 timeformat = '%H:%M:%S,%f' # Time format for parsing SubRip timecodes
+prevkey = -1 # Previous subtitle key
+prevline = '' # Previous subtitle line
 workingpath = os.getcwd() # Working path
 zerostamp = '00:00:00,000' # Zero time stamp
 
@@ -35,23 +37,24 @@ config.read(configfile)
 blacklist = str(config.get("Configuration", "BlackList"))
 blacklist_regex = re.compile(blacklist)
 check_blacklist = config.getboolean("Configuration", "CheckBlackList")
+check_duplicates = config.getboolean("Configuration", "CheckDuplicates")
 check_numbering = config.getboolean("Configuration", "CheckNumbering")
 check_overlapping = config.getboolean("Configuration", "CheckOverlapping")
-min_duration = str(config.get("Configuration", "MinimumDuration"))
+min_duration = int(config.get("Configuration", "MinimumDuration"))
 
 # Read command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", action="store_true", dest="debug",
-    required=False, help="Print debugging information.")
+  required=False, help="Print debugging information.")
 parser.add_argument("-f", action="store_true", dest="fix",
-    required=False, help="Fix file.")
+  required=False, help="Fix file.")
 args = parser.parse_args() # Returns data from the options specified
 
 # Command line arguments override default and config settings
 if args.debug:
-    debug = True
+  debug = True
 if args.fix:
-    fix = True
+  fix = True
 
 
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -61,144 +64,165 @@ if args.fix:
 # List files from working path
 for filename in os.listdir(workingpath):
 
-    ext = os.path.splitext(filename)[1][1:].lower()
-    filepath = os.path.join(workingpath, filename)
+  ext = os.path.splitext(filename)[1][1:].lower()
+  filepath = os.path.join(workingpath, filename)
 
-    # Check file extension
-    if ext != 'srt':
-        continue
+  # Check file extension
+  if ext != 'srt':
+    continue
 
-    # Read SubRip file
-    print('Processing file: "{0}".'.format(filename))
+  # Read SubRip file
+  print('Processing file: "{0}".'.format(filename))
+  print()
+  srt = SubRip()
+  ok, message = srt.open_file(filepath)
+  if not ok:
+    print('Error while opening file: "{0}"'.format(filename))
+    print(message)
+    sys.exit()
+
+  # Print subtitle data for debugging purposes
+  if debug:
+    for subtitle in srt.subtitles:
+      print(subtitle)
     print()
-    srt = SubRip()
-    ok, message = srt.open_file(filepath)
-    if not ok:
-        print('Error while opening file: "{0}"'.format(filename))
-        print(message)
-        sys.exit()
 
-    # Print subtitle data for debugging purposes
-    if debug:
-        for subtitle in srt.subtitles:
-            print(subtitle)
+
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # Check blacklisted words
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+  if check_blacklist:
+    for key, subtitle in enumerate(srt.subtitles):
+      r = blacklist_regex.search(subtitle[2])
+      if r is not None:
+        print('Blacklisted word found from line:')
+        print('Line number..........: {0}'.format(key))
+        print('Word.................: {0}'.format(r.group(0)))
+        print('Subtitles............: {0}.'.format(subtitle[2]))
+        print()
+        srt.delete(key)
+
+
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # Check duplicate lines
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+  if check_duplicates:
+    key = len(srt.subtitles) - 1
+    for subtitle in reversed(srt.subtitles):
+
+      # Check if current line is a duplicate of previous line
+      if prevline == subtitle[2]:
+        print('Duplicate subtitle lines detected.')
+        print('Previous line...: {0}'.format(prevline))
+        print('Current line....: {0}'.format(subtitle[2]))
+        if fix:
+          print('Removed duplicate subtitle. Line: "{0}".'.format(subtitle[2]))
+          srt.subtitles[key][1] = subtitle[1][0:12] + ' --> ' + prevend
+          srt.delete(key + 1)
         print()
 
-
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # Check blacklisted words
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-    if check_blacklist:
-        for key, subtitle in enumerate(srt.subtitles):
-            r = blacklist_regex.search(subtitle[2])
-            if r is not None:
-                print('Blacklisted word found from line:')
-                print('Line number..........: {0}'.format(key))
-                print('Word.................: {0}'.format(r.group(0)))
-                print('Subtitles............: {0}.'.format(subtitle[2]))
-                print()
-                srt.delete(key)
+      # Set current subtitle as previous subtitle for the next loop cycle
+      prevend = subtitle[1][-12:]
+      prevline = subtitle[2]
+      key -= 1
 
 
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # Check subtitle numbering
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # Check subtitle numbering
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-    if check_numbering:
-        counter = 1
-        for key, subtitle in enumerate(srt.subtitles):
-            subtitle_number = int(subtitle[0])
-            if counter != subtitle_number:
-                print('Subtitle numbering error in file: "{0}".'.format(
-                    filename))
-                print('Expected number {0}, got {1}.'.format(
-                    counter, subtitle_number))
-                if fix:
-                    srt.subtitles[key][0] = str(counter)
-                    print('Fixed numbering. Line="{0}".'.format(
-                        srt.subtitles[key]))
-                print()
-            counter += 1
+  if check_numbering:
+    counter = 1
+    for key, subtitle in enumerate(srt.subtitles):
+      subtitle_number = int(subtitle[0])
+      if counter != subtitle_number:
+        print('Subtitle numbering error in file: "{0}".'.format(filename))
+        print('Expected number {0}, got {1}.'.format(counter, subtitle_number))
+        if fix:
+          srt.subtitles[key][0] = str(counter)
+          print('Fixed numbering. Line="{0}".'.format(srt.subtitles[key]))
+        print()
+      counter += 1
 
 
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # Check subtitle overlapping
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # Check subtitle overlapping
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-    if check_overlapping:
+  if check_overlapping:
 
-        # Reset variables
-        datetime3 = datetime.datetime.strptime(zerostamp, timeformat)
-        datetime4 = datetime.datetime.strptime(zerostamp, timeformat)
-        prevkey = -1
-        timecode = re.compile(r'^(\d{2}:\d{2}:\d{2},\d{3}) --> ' +
-            '(\d{2}:\d{2}:\d{2},\d{3})\Z')
+    # Reset variables
+    datetime3 = datetime.datetime.strptime(zerostamp, timeformat)
+    datetime4 = datetime.datetime.strptime(zerostamp, timeformat)
+    prevkey = -1
+    timecode = re.compile(r'^(\d{2}:\d{2}:\d{2},\d{3}) --> ' +
+        '(\d{2}:\d{2}:\d{2},\d{3})\Z')
 
-        for key, subtitle in enumerate(srt.subtitles):
+    for key, subtitle in enumerate(srt.subtitles):
 
-            datetime1 = datetime.datetime.strptime(zerostamp, timeformat)
-            datetime2 = datetime.datetime.strptime(zerostamp, timeformat)
-            linenumber = key + 1
+      datetime1 = datetime.datetime.strptime(zerostamp, timeformat)
+      datetime2 = datetime.datetime.strptime(zerostamp, timeformat)
+      linenumber = key + 1
 
-            # Parse timecodes into datetime objects
-            r = timecode.search(subtitle[1])
-            if r is not None:
-                datetime1 = datetime.datetime.strptime(r.group(1), timeformat)
-                datetime2 = datetime.datetime.strptime(r.group(2), timeformat)
-                timestr1=datetime1.strftime(timeformat)[:-3]
-                timestr2=datetime2.strftime(timeformat)[:-3]
+      # Parse timecodes into datetime objects
+      r = timecode.search(subtitle[1])
 
-                # Check if subtitle show/hide-timecodes are ok
-                if datetime1 >= datetime2:
-                    datetime5 = datetime1 + datetime.timedelta(
-                        milliseconds=min_duration)
-                    timestr5=datetime5.strftime(timeformat)[:-3]
-                    print('Show/hide subtitle overlapping detected.')
-                    print('Linenumber...........: {0}, line: {1}'.format(
-                        linenumber, subtitle[1]))
-                    print('Fixed timestamp......: {0}, line: {1} --> {2}'
-                        .format(linenumber, timestr1, timestr5))
-                    print()
-                    if fix:
-                        srt.subtitles[key][1] = timestr1 + ' --> ' + timestr5
+      if r is not None:
+        datetime1 = datetime.datetime.strptime(r.group(1), timeformat)
+        datetime2 = datetime.datetime.strptime(r.group(2), timeformat)
+        timestr1=datetime1.strftime(timeformat)[:-3]
+        timestr2=datetime2.strftime(timeformat)[:-3]
 
-                # Check if previous subtitle hide-timecode is ok
-                elif datetime1 <= datetime4:
-                    datetime5 = datetime1 - datetime.timedelta(milliseconds=1)
-                    timestr5=datetime5.strftime(timeformat)[:-3]
-                    print('Subtitle overlapping detected between previous',
-                        'subtitle.')
-                    print('Previous timestamp...: {0}, line: {1}'
-                        .format(prevkey, srt.subtitles[prevkey][1]))
-                    print('Current timestamp....: {0}, line: {1}'.format(
-                        linenumber, subtitle[1]))
-                    print('Fixed timestamp......: {0}, line: {1} --> {2}'
-                        .format(prevkey, timestr3, timestr5))
-                    print()
-                    if fix:
-                        srt.subtitles[prevkey][1] = timestr3 + ' --> ' + timestr5
+        # Check if subtitle show/hide-timecodes are ok
+        if datetime1 >= datetime2:
+          datetime5 = datetime1 + datetime.timedelta(milliseconds=min_duration)
+          timestr5=datetime5.strftime(timeformat)[:-3]
+          print('Show/hide subtitle overlapping detected.')
+          print('Linenumber...........: {0}, line: {1}'.format(
+            linenumber, subtitle[1]))
+          print('Fixed timestamp......: {0}, line: {1} --> {2}'
+            .format(linenumber, timestr1, timestr5))
+          print()
+          if fix:
+            srt.subtitles[key][1] = timestr1 + ' --> ' + timestr5
 
-                # Save previous line data for later checks
-                datetime3 = datetime.datetime.strptime(r.group(1), timeformat)
-                datetime4 = datetime.datetime.strptime(r.group(2), timeformat)
-                timestr3=datetime3.strftime(timeformat)[:-3]
-                timestr4=datetime4.strftime(timeformat)[:-3]
-                prevkey = key
+        # Check if previous subtitle hide-timecode is ok
+        elif datetime1 <= datetime4:
+          datetime5 = datetime1 - datetime.timedelta(milliseconds=1)
+          timestr5=datetime5.strftime(timeformat)[:-3]
+          print('Subtitle overlapping detected between previous',
+            'subtitle.')
+          print('Previous timestamp...: {0}, line: {1}'
+            .format(prevkey, srt.subtitles[prevkey][1]))
+          print('Current timestamp....: {0}, line: {1}'.format(
+            linenumber, subtitle[1]))
+          print('Fixed timestamp......: {0}, line: {1} --> {2}'
+            .format(prevkey, timestr3, timestr5))
+          print()
+          if fix:
+            srt.subtitles[prevkey][1] = timestr3 + ' --> ' + timestr5
 
-            else:
-                print('Unable to extract timecodes from file: "{0}".'.format(
-                    filename))
-                continue
+        # Save previous line data for later checks
+        datetime3 = datetime.datetime.strptime(r.group(1), timeformat)
+        datetime4 = datetime.datetime.strptime(r.group(2), timeformat)
+        timestr3=datetime3.strftime(timeformat)[:-3]
+        timestr4=datetime4.strftime(timeformat)[:-3]
+        prevkey = key
+
+      else:
+        print('Unable to extract timecodes from file: "{0}".'.format(filename))
+        continue
 
 
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    # File processing complete. Save file if option is enabled.
-    #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # File processing complete. Save file if option is enabled.
+  #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-    if fix:
-        print("File saved with fixes.")
-        srt.save_file()
+  if fix:
+    print("File saved with fixes.")
+    srt.save_file()
 
-    print('Processing complete.')
-    print()
+  print('Processing complete.')
+  print()
--- class_subrip.py
+++ class_subrip.py
@@ -1,129 +1,159 @@
 # !/usr/bin/env python3
 
-""" A class to handle SubRip (.srt) files. """
+''" A class to handle SubRip (.srt) files. ''"
 
 # Imports
 import os # Miscellaneous operating system interfaces
 import re # Regular expression operations
 import sys # System-specific parameters and functions
 
+
 # A class to handle SubRip files
 class SubRip(object):
 
-    def __init__(self):
+  # Initialization
+  def __init__(self):
 
-        # Declare class variables
-        self.filepath = '' # Currently open file path
-        self.subtitles = [] # A list for subtitles
+    # Declare class variables
+    self.filepath = '' # Currently open file path
+    self.subtitles = [] # A list for subtitles
 
-        # Regular expression to match timecodes
-        self.timecode = re.compile(r'^\d{2}:\d{2}:\d{2},\d{3} --> ' +
-            '\d{2}:\d{2}:\d{2},\d{3}\Z')
-
-
-    def close_file(self):
-        self.filepath = ''
-        self.subtitles = []
+    # Regular expression to match timecodes
+    self.timecode = re.compile(r'^\d{2}:\d{2}:\d{2},\d{3} --> ' +
+      '\d{2}:\d{2}:\d{2},\d{3}\Z')
 
 
-    def delete(self, key):
-        del self.subtitles[key]
+  # Close SubRip-file
+  def close_file(self):
+    self.filepath = ''
+    self.subtitles = []
 
 
-    def open_file(self, filepath, enc='utf_8_sig'):
-
-        # Close any previous file
-        self.close_file()
-
-        # Check file path
-        if filepath == '':
-            return False, 'File not specified.'
-
-        # Retrieve basename for file
-        basename = os.path.basename(filepath)
-
-        # Check if file exists
-        if not os.path.exists(filepath):
-            return False, 'File does not exist: "{0}".'.format(basename)
-
-        # File path should be an existing regular file
-        if not os.path.isfile(filepath):
-            return False, 'Not a file: "{0}".'.format(basename)
-
-        # File should not be empty
-        if not os.path.getsize(filepath) > 0:
-            return False, 'File is empty: "{0}".'.format(basename)
-
-        # Check file extension
-        ext = os.path.splitext(filepath)[1][1:].lower()
-        if ext != 'srt':
-            return False, 'File type is not valid: "{0}".'.format(basename)
-
-        # Loop through file and parse contents into a list
-        key = ""
-        timecode = ""
-        lines = ""
-        i = 0
-        with open(filepath, 'r', encoding=enc) as filehandle:
-            for line in iter(filehandle):
-                i +=1
-                line = line.strip()
-                if key == "":
-                    if re.match(r"^[\d]+\Z", line):
-                        key = line
-                    else:
-                        return (False, 'Numbering error. Line="{0}".'
-                            .format(line))
-                elif timecode == "":
-                    if self.timecode.match(line):
-                        timecode = line
-                    else:
-                        return (False, 'Timecode error. Line="{0}".'
-                            .format(line))
-                elif line != "":
-                    if lines != "":
-                        lines += "<br />"
-                    lines += line
-                else:
-                    self.subtitles.append([key, timecode, lines])
-                    key = ""
-                    timecode = ""
-                    lines = ""
-                    print("i=",i,"/line =",line,"===",[key, timecode, lines])
-            self.subtitles.append([key, timecode, lines])
-        self.filepath = filepath
-        return True, 'File opened successfully.'
+  # Delete subtile
+  def delete(self, key):
+    del self.subtitles[key]
 
 
-    def save_file(self, enc='utf_8_sig'):
+  # Open SubRip-file
+  def open_file(self, filepath, enc='utf_8_sig'):
 
-        # Check for file
-        if not self.filepath:
-            return False, 'No file open.'
+    # Close any previous file
+    self.close_file()
 
-        # Remove empty lines from the end of the file
-        length = int(len(self.subtitles))
-        while length > 1 and self.subtitles[length - 1] == '':
-            del self.subtitles[length - 1]
-            length = int(len(self.subtitles))
+    # Check file path
+    if filepath == '':
+      return False, 'File not specified.'
 
-        # Loop file data and save to file
-        try:
-            filehandle = open(self.filepath, 'w', encoding=enc)
-            for key, subtitle in enumerate(self.subtitles):
-                if key > 0:
-                    filehandle.write('\n')
-                filehandle.write(subtitle[0] + '\n')
-                filehandle.write(subtitle[1] + '\n')
-                filehandle.write(subtitle[2].replace('<br />', '\n') + '\n')
-            filehandle.close()
-        except IOError as e:
-            return False, 'I/O error({0}): "{1}".'.format(e.errno, e.strerror)
-        except UnicodeDecodeError:
-            return False, 'Unicode Decode Error: "{0}".'.format(filepath)
-        except UnicodeEncodeError:
-            return False, 'Unicode Encode Error: "{0}".'.format(filepath)
-        except:
-            return False, 'Unexpected error: "{0}".'.format(sys.exc_info()[1])
+    # Retrieve basename for file
+    basename = os.path.basename(filepath)
+
+    # Check if file exists
+    if not os.path.exists(filepath):
+      return False, 'File does not exist: "{0}".'.format(basename)
+
+    # File path should be an existing regular file
+    if not os.path.isfile(filepath):
+      return False, 'Not a file: "{0}".'.format(basename)
+
+    # File should not be empty
+    if not os.path.getsize(filepath) > 0:
+      return False, 'File is empty: "{0}".'.format(basename)
+
+    # Check file extension
+    ext = os.path.splitext(filepath)[1][1:].lower()
+    if ext != 'srt':
+      return False, 'File type is not valid: "{0}".'.format(basename)
+
+    # Loop through file and parse contents into a list
+    key = ''
+    timecode = ''
+    lines = ''
+    with open(filepath, 'r', encoding=enc) as filehandle:
+      for line in iter(filehandle):
+
+        # Trim line
+        line = line.strip()
+
+        # First we find out the subtitle key
+        if key == '':
+          if re.match(r"^[\d]+\Z", line):
+            key = line
+            continue
+          else:
+            return False, 'Numbering error. Line="{0}".'.format(line)
+
+        # After subtitle key, next thing should be the timestamps
+        if key != '' and timecode == '':
+          if self.timecode.match(line):
+            timecode = line
+            continue
+          else:
+            return False, 'Timecode error. Line="{0}".'.format(line)
+
+        # The rest of the lines are the actual subtitle lines
+        if key != '' and timecode != '' and line != '':
+          if lines != '':
+            lines += '<br />'
+          lines += line
+          continue
+
+        # When we have everything, we append it to our subtitles list
+        if key != '' and timecode != '' and lines != '':
+          self.subtitles.append([key, timecode, lines])
+          key = ''
+          timecode = ''
+          lines = ''
         else:
-            return True, 'File saved successfully.'
+          return False, 'Subtitle parsing error. Key="{0}", ' + \
+            'timecode="{1}", lines="{2}".'.format(key, timecode, lines)
+
+    # The loop doesn't always catch the last line if the file has only one
+    # empty line at EOF
+    if key != '' and timecode != '' and lines != '':
+      self.subtitles.append([key, timecode, lines])
+      key = ''
+      timecode = ''
+      lines = ''
+
+    # Final check so variables are completely empty
+    if key == '' and timecode == '' and lines == '':
+      self.filepath = filepath
+      return True, 'File opened successfully.'
+    else:
+      return False, 'Subtitle parsing error. Key="{0}", ' + \
+        'timecode="{1}", lines="{2}".'.format(key, timecode, lines)
+
+
+  # Save SubRip-file
+  def save_file(self, enc='utf_8_sig'):
+
+    # Check for file
+    if not self.filepath:
+      return False, 'No file open.'
+
+    # Remove empty lines from the end of the file
+    length = int(len(self.subtitles))
+    while length > 1 and self.subtitles[length - 1] == '':
+      del self.subtitles[length - 1]
+      length = int(len(self.subtitles))
+
+    # Loop file data and save to file
+    try:
+      filehandle = open(self.filepath, 'w', encoding=enc)
+      for key, subtitle in enumerate(self.subtitles):
+        if key > 0:
+          filehandle.write('\n')
+        filehandle.write(subtitle[0] + '\n')
+        filehandle.write(subtitle[1] + '\n')
+        filehandle.write(subtitle[2].replace('<br />', '\n') + '\n')
+      filehandle.close()
+    except IOError as e:
+      return False, 'I/O error({0}): "{1}".'.format(e.errno, e.strerror)
+    except UnicodeDecodeError:
+      return False, 'Unicode Decode Error: "{0}".'.format(filepath)
+    except UnicodeEncodeError:
+      return False, 'Unicode Encode Error: "{0}".'.format(filepath)
+    except:
+      return False, 'Unexpected error: "{0}".'.format(sys.exc_info()[1])
+    else:
+      return True, 'File saved successfully.'
